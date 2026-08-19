8b1d54be8132e51a3d7070875e317f4e7653e349
jiayouchew <jiayoucheww@gmail.com>
2026-08-19 19:31:59 +0800

    Implemented Level-0: Rename, Greet, Exit

--- src/main/java/Wobble.java
+++ src/main/java/Wobble.java
@@ -1,10 +1,14 @@
 public class Wobble {
     public static void main(String[] args) {
-        String banner = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println(banner);
+        System.out.println("==============================");
+        System.out.println("  WOBBL-E // systems online");
+        System.out.println("==============================");
+        System.out.println("Hello! I'm Wobble.");
+        System.out.println("Beep boop! Your friendly little robot companion is ready.");
+        System.out.println("What can I do for you?");
+        System.out.println("==============================");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("Wobble powering down... beep!");
+        System.out.println("==============================");
     }
 }
